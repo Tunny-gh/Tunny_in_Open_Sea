d9fe2c708b9e61eddead3aac4a98e5dddb8ab5a7
hrntsm <contact@hrntsm.com>
2025-02-11 18:52:39 +0900

    Add platform-specific Python path handling for Windows and macOS

--- Tunny.Core/Util/TEnvVariables.cs
+++ Tunny.Core/Util/TEnvVariables.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 namespace Tunny.Core.Util
 {
@@ -13,8 +14,42 @@ namespace Tunny.Core.Util
         public static string QuitFishingPath { get; } = Path.Combine(TunnyEnvPath, "quit.fishing");
         public static string DesignExplorerPath { get; } = Path.Combine(TunnyEnvPath, "TT-DesignExplorer");
         public static string OptimizeSettingsPath { get; } = Path.Combine(TunnyEnvPath, "settings.json");
-        public static string PythonPath { get; } = Path.Combine(TunnyEnvPath, "python");
-        public static string PythonDllPath { get; } = Path.Combine(PythonPath, @"python312.dll");
+        public static string PythonPath
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return Path.Combine(TunnyEnvPath, "python");
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".rhinocode", "py39-rh8");
+                }
+                else
+                {
+                    throw new PlatformNotSupportedException();
+                }
+            }
+        }
+        public static string PythonDllPath
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return Path.Combine(PythonPath, @"python312.dll");
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    return Path.Combine(PythonPath, "libpython3.9.dylib");
+                }
+                else
+                {
+                    throw new PlatformNotSupportedException();
+                }
+            }
+        }
         public static string ComponentFolder { get; } = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         public static string ExampleDirPath { get; } = Path.Combine(ComponentFolder, "Examples", "Grasshopper");
         public static Version OldStorageVersion { get; } = new Version("0.9.1");
