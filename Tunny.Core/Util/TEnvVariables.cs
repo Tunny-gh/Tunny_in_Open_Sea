b036174ea36f29d16a72e90ac2be66372b50999e
hrntsm <contact@hrntsm.com>
2025-02-11 19:28:43 +0900

    Add support for optuna-dashboard path handling in TEnvVariables

--- Tunny.Core/Util/TEnvVariables.cs
+++ Tunny.Core/Util/TEnvVariables.cs
@@ -14,6 +14,10 @@ namespace Tunny.Core.Util
         public static string QuitFishingPath { get; } = Path.Combine(TunnyEnvPath, "quit.fishing");
         public static string DesignExplorerPath { get; } = Path.Combine(TunnyEnvPath, "TT-DesignExplorer");
         public static string OptimizeSettingsPath { get; } = Path.Combine(TunnyEnvPath, "settings.json");
+        public static string ComponentFolder { get; } = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        public static string ExampleDirPath { get; } = Path.Combine(ComponentFolder, "Examples", "Grasshopper");
+        public static Version OldStorageVersion { get; } = new Version("0.9.1");
+        public static IntPtr GrasshopperWindowHandle { get; set; }
         public static string PythonPath
         {
             get
@@ -50,10 +54,24 @@ namespace Tunny.Core.Util
                 }
             }
         }
-        public static string ComponentFolder { get; } = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        public static string ExampleDirPath { get; } = Path.Combine(ComponentFolder, "Examples", "Grasshopper");
-        public static Version OldStorageVersion { get; } = new Version("0.9.1");
-        public static IntPtr GrasshopperWindowHandle { get; set; }
+        public static string DashboardPath
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return Path.Combine(PythonPath, "Scripts", "optuna-dashboard.exe");
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    return Path.Combine(PythonPath, "bin", "optuna-dashboard");
+                }
+                else
+                {
+                    throw new PlatformNotSupportedException();
+                }
+            }
+        }
 
         public static string TmpDirPath
         {
