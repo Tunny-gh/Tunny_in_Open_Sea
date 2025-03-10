3a609741a91c7733ddb08425d8422e56a290ca85
NATSUME Hiroaki <hiroaki@nature-architects.com>
2025-03-10 22:17:03 +0900

    Add unit tests for AutoSampler functionality

---
+++ OptunaTests/Sampler/OptunaHub/AutoSamplerTests.cs
@@ -0,0 +1,36 @@
+using Optuna.Util;
+
+using OptunaTests;
+
+using Xunit;
+
+namespace Optuna.Sampler.OptunaHub.Tests
+{
+    public class AutoSamplerTests : IClassFixture<TestFixture>
+    {
+        private readonly TestFixture _fixture;
+
+        public AutoSamplerTests(TestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void ConstructorTest()
+        {
+            var sampler = new AutoSampler();
+            Assert.Equal(ObjectiveNumberSupport.Any, sampler.ObjectiveNumberSupport);
+            Assert.False(sampler.ForceReload);
+        }
+
+        [Fact]
+        public void ToPythonTest()
+        {
+            var sampler = new AutoSampler();
+            dynamic pyObj = sampler.ToPython(false);
+            Assert.Equal("AutoSampler", pyObj.ToString());
+            dynamic pyObj2 = sampler.ToPython(true);
+            Assert.Equal("AutoSampler", pyObj2.ToString());
+        }
+    }
+}
