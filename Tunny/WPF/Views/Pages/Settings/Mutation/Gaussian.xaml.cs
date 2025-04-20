1af329f9068d67c0f18ef101a148a2cfa60808b7
hrntsm <contact@hrntsm.com>
2025-04-20 20:47:01 +0900

    Add Gaussian and Uniform mutation pages with initial layout

---
+++ Tunny/WPF/Views/Pages/Settings/Mutation/Gaussian.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using Tunny.Core.Input;
+
+namespace Tunny.WPF.Views.Pages.Settings.Mutation
+{
+    public partial class Gaussian : Page
+    {
+        public Gaussian()
+        {
+            InitializeComponent();
+        }
+
+        private void SigmaFactorTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            string value = textBox.Text;
+            textBox.Text = InputValidator.IsPositiveDouble(value, false) ? value : "0.033";
+        }
+    }
+}
---
+++ Tunny/WPF/Views/Pages/Settings/Mutation/Uniform.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Tunny.WPF.Views.Pages.Settings.Mutation
+{
+    public partial class Uniform : Page
+    {
+        public Uniform()
+        {
+            InitializeComponent();
+        }
+    }
+}
